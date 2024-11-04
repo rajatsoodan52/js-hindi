337893023fadb63d0e5bf4239a3735ec068ae805
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-04 07:17:42 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+// alert(3+3)// we are using nodejs, not browser
+ 
+console.log(3+
+3) //code readability should be high so dont use it//
+console.log("Hitesh")
+let name="reitsh"
+let age=18;
+let isLoggedIn=false;
+let state =null;
+// number =>2 to power 53
+//bigint
+// string can be single and double quote string=" "
+// boolean =>true/false
+//null => standalone value
+// undefined =>
+// symbol
+
+//object
+
+
+console.log(typeof "raj") 
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
