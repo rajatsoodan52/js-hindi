9b13f6149d49f2e10b147ed7c2bf1751b292aca1
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-05 18:01:24 +0000

    new file:   01_basics/operations.js  	modified:   01_basics/03_conversionOperstor.js

---
+++ 01_basics/operations.js
@@ -0,0 +1,24 @@
+let value=3;
+let negValue=-value
+console.log(negValue) 
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2") 
+console.log(+ true);
+
+//console.log(2>1)
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0);
+
+console.log("2"===2);
