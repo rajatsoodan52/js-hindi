f9563395adbc49f704ed95b5a12f117ca02fd4f8
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-05 16:36:07 +0000

    conversionofDatatypes

---
+++ 01_basics/03_conversionOperstor.js
@@ -0,0 +1,15 @@
+let score="33abc ";
+console.log(typeof(score))
+console.log(typeof score)
+ 
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+//"33" =>33
+//"33abc" =>NaN
+// true =>1 ; false=>0
+  
+// 1=>true; 0=>false;
+// "" =>false;
+//"hitesh" =>true 
+let someNumber=33
+let stringNumber=String(someNumber)
