9bba51c417dbe1c643a92b49fb9019d064cd9380
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-06 05:23:15 +0000

    datatype summary

---
+++ datatpyesnotes.js
@@ -0,0 +1,38 @@
+// Primitive
+ 
+// 7 types: String, Number,Boolean,null,undefined,Symbol,BigInt
+
+//const score=100, const score=false-
+
+
+const score=100;
+const scoreValue=100.3
+
+const isLoogedIn=false;
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber=54454547567766n
+
+
+//Reference (Non Primitive)
+
+//Array,Objects,functions
+//array declaration
+const heros=["shaktiman","naagraj","doga"]
+//object creation  
+ let myObj={
+    name:"raj",
+    age: 22,
+}
+
+const myFunction=function(){
+    console.log("helllo world"); 
+}
+
+console.log(typeof bigNumber);
