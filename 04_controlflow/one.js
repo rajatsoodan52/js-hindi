dc5671a0fd3cf5e037172e403b3f586d147836df
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-13 11:02:32 +0000

    new file:   04_controlflow/one.js 	new file:   04_controlflow/two.js

---
+++ 04_controlflow/one.js
@@ -0,0 +1,52 @@
+// if
+
+// const isUserLoggedIn=true
+// if(2!=3){
+//    console.log("executed"); 
+// }
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+//<, >, <= ,>=,==,!=,===,!==
+
+const balance=1000
+
+//if(balance>500) console.log("test");//for ex to write multiple lines just write , and write another statement
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// }
+// else if(balance< 900) {
+//     console.log("less than 1200")
+// }
+// else{
+//     console.log("less tjsn 3mm");
+//     }
+
+ const  isUserLoggedIn=true
+ const debitCard=true
+ const loggedInFromGoogle=false
+ const loggedInFromEmail=true
+
+ if(isUserLoggedIn && debitCard){
+    console.log("Allow to buy course");
+    
+ }
+
+ if(loggedInFromEmail|| loggedInFromGoogle){
+    console.log("User logged in")
+ }
+
+
+    
+
---
+++ 04_controlflow/two.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+ 
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 1:
+        console.log("march");
+        break;
+    case 1:
+        console.log("April");
+        break;
+
+
+    default:
+        break;
+}
