7364f7c2a5c646aab3dfaf29c76ee7b5d3023a42
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-15 04:33:14 +0000

    modified:   loopsiterators.js/01.js

--- loopsiterators.js/01.js
+++ loopsiterators.js/01.js
@@ -22,7 +22,25 @@ let myArray=["flash","batman","sperman"]
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
     console.log(element);
+}
+
+// break , continue
+// for (let index = 0; index <=20; index++) {
+//     if(index==5){
+//         console.log('Detected 5')
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
+   
     
+// }
+for (let index = 0; index <=20; index++) {
+    if(index==5){
+        console.log('Detected 5')
+        continue;
+    }
+   console.log(`Value of i is ${index}`);
+   
     
 }
 
