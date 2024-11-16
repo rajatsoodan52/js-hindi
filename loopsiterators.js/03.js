9f0459f74e8c84d21a6b7be8f9bd43b38a7129a7
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-16 06:56:28 +0000

    new file:   loopsiterators.js/03.js

---
+++ loopsiterators.js/03.js
@@ -0,0 +1,45 @@
+// for of 
+
+// ["","",""]
+
+// [{},{},{}]
+
+const arr=[1,2,3,4,5]
+
+for (const num of arr) { //objects are like on which we have to apply the loop
+    console.log(num);
+    }
+
+const greetings="hello world"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+}
+
+// Maps
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"United States Of America")
+map.set('IN',"India")
+map.set('Fr',"France")
+// console.log(map)
+
+// for(const key of map){
+//     console.log(key);  outputs key as well as the values  as  a whole
+    
+// }
+
+// for (const [key,value] of map) {// here this [key,value] this is called as the destructuring of the array
+//     // and now we can use them individually
+//     console.log(key,':-', value);
+// }
+
+// const myObject={
+//     game1:'NFS',
+//     game2:'Spiderman' // object is not iteratable like this
+// }
+
+// for (const [key,value] of object) {
+//     console.log(key,':-',value);
+    
+// } error:- Objects are not iteratable
