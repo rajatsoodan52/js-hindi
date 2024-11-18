ee1ee1116472e40cc04a7dcee8ab66e481045914
rajatsoodan52 <rajatsoodancse2024@gmail.com>
2024-11-18 18:07:01 +0000

    modified:   loopsiterators.js/06.js

--- loopsiterators.js/06.js
+++ loopsiterators.js/06.js
@@ -1,6 +1,26 @@
 const myNums=[1,2,3,4,5,6,7,8,9,10]
 
-const newNums=myNums.filter((num) =>{
-  return num>4
-})
-console.log(newNums);
+// const newNums=myNums.filter((num) =>{
+//   return num>4
+// })
+// console.log(newNums); // It returns a new array of elements
+
+// const newNums=[]
+
+// myNums.forEach((num)=>{
+//   if(num>4){
+//     newNums.push(num)
+//   }
+// })
+
+// console.log(newNums);
+
+
+// let userBooks=books.filter((bk) => bk.genre ==='History') //scope npt openrde here
+
+// const userBooks=books.filter((bk)=>{
+  // scope is opened here
+//   bk.publish >=2000
+// })
+// console.log(userBooks);
+
